30d3ca1780f2944fedc21afdf8185a7cdc7e750e
yuki takagi <yuki99takagi@gmail.com>
2020-05-11 12:39:15 +0900

    [Add] jobの設定

--- app/jobs/titles_change_job.rb
+++ app/jobs/titles_change_job.rb
@@ -1,7 +1,6 @@
 class TitlesChangeJob < ApplicationJob
   queue_as :default
-
-  def perform(*args)
-    # Do something later
+  def perform(blog_id)
+    Blog.find(blog_id).title_change
   end
 end
