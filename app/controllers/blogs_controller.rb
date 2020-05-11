2a8f29f8dc1bcee3076299af105b2051bd067736
yuki takagi <yuki99takagi@gmail.com>
2020-05-11 12:32:39 +0900

    [Add] gem 'delayed_job_active_record'

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -28,6 +28,7 @@ class BlogsController < ApplicationController
 
     respond_to do |format|
       if @blog.save
+        @blog.title_change
         format.html { redirect_to @blog, notice: 'Blog was successfully created.' }
         format.json { render :show, status: :created, location: @blog }
       else
